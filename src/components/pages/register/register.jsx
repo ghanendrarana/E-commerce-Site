9d8bf18497246029a7b128bafb5eee0121be9d85
Ghanendra Rana <ghanendrarana.dev@gmail.com>
2025-07-08 13:25:49 +0545

    Registration page added with simple styling

--- src/components/pages/register/register.jsx
+++ src/components/pages/register/register.jsx
@@ -1,29 +1,39 @@
-
 import Header from "../../layout/header/header";
 
-export default function Register () {
+import styles from "./register.module.css";
 
-    return (
-        <div>
-            <Header />
-            <div>
-                <div>Create Account</div>
-                <form>
-                    <div>First Name</div>
-                    <input type="text" placeholder="Enter your first name" />
-                    <div>Last Name</div>
-                    <input type="text" placeholder="Enter your last name" />
-                    <div>Email</div>
-                    <input type="email" placeholder="Enter your email" />
-                    <div>Password</div>
-                    <input type="password" placeholder="Enter your password" />
-                    <div>Confirm Password</div>
-                    <input type="password" placeholder="Confirm your password" />
-                    <div>
-                        <button type="submit">Register</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+export default function Register() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.registrationPage}>
+        <div>Create Account</div>
+        <form className={styles.registrationForm}>
+          <div>
+            <div>First Name</div>
+            <input type="text" placeholder="Enter your first name" />
+          </div>
+          <div>
+            <div>Last Name</div>
+            <input type="text" placeholder="Enter your last name" />
+          </div>
+          <div>
+            <div>Email</div>
+            <input type="email" placeholder="Enter your email" />
+          </div>
+          <div>
+            <div>Password</div>
+            <input type="password" placeholder="Enter your password" />
+          </div>
+          <div>
+            <div>Confirm Password</div>
+            <input type="password" placeholder="Confirm your password" />
+          </div>
+          <div>
+            <button type="submit">Register</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
