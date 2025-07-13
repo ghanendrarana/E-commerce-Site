fe40c7dd72d7e0ae36bac5a9f8caedc691e0b5c0
Ghanendra Rana <ghanendrarana.dev@gmail.com>
2025-07-13 22:15:01 +0545

    Search box code added but need to do full work

--- src/components/userAction/search/search.jsx
+++ src/components/userAction/search/search.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import { Search } from 'lucide-react';
+import React, { useState } from "react";
+import { Search } from "lucide-react";
 
-import styles from './search.module.css';
+import styles from "./search.module.css";
 
 export default function SearchField() {
-    return (
-        <button  className={styles.search}>
-            <Search  />
-        </button>
-    );
+  const [showInput, setShowInput] = useState(false);
+  const [query, setQuery] = useState("");
+
+  //it stops refreshing of websit each time when clicked
+  function handleSearch(e) {
+    e.preventDefault();
+    console.log(query);
+  }
+
+  // to change showInput states from true and false
+  function toggleInput() {
+    setShowInput((prev) => !prev);
+  }
+
+  return (
+    <form onSubmit={handleSearch} className={styles.searchField}>
+      {showInput && (
+        <input
+          type="text"
+          name="searchQuery"
+          value={query}
+          placeholder="Search Products"
+          onChange={(e) => setQuery(e.target.value)}
+          className={styles.searchInput}
+        />
+      )}
+      <button
+        type={showInput ? "Submit" : "button"}
+        onClick={showInput ? null : toggleInput}
+        className={styles.searchBtn}
+      >
+        <Search />
+      </button>
+    </form>
+  );
 }
