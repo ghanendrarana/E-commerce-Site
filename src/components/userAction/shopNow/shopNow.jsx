9af5941cb124ac2f9a1c2cfbb57c0714c623c826
Ghanendra Rana <ghanendrarana.dev@gmail.com>
2025-08-03 12:18:28 +0545

    Css added in chopNow page

--- src/components/userAction/shopNow/shopNow.jsx
+++ src/components/userAction/shopNow/shopNow.jsx
@@ -1,31 +1,37 @@
-import React from "react" 
+import React from "react";
 import Header from "../../layout/header/header";
 import useProductStore from "../../../store/useProductStore";
 
 import styles from "./shopNow.module.css";
 
-export default function ShopNow () {
-    const product = useProductStore((state) => state.selectedProduct);
-     console.log(product);
-    if (!product) return <p>No Product Selected</p>
+export default function ShopNow() {
+  const product = useProductStore((state) => state.selectedProduct);
+  console.log(product);
+  if (!product) return <p>No Product Selected</p>;
 
-    return (
-        <div>
-            <Header />
-            <div className={styles.mainContainer}>
-                <div className={styles.detailsContainer}>
-                    <img src={product.image} />
-                    <div>
-                        <div className={styles.productName}>{product.name}</div>
-                        <div className={styles.productPrice}>{product.price}</div>
-                        <div>{product.description}</div>
-                        <div>Size</div>
-                        <div>Color</div>
-                        <div>Quantity</div>
-                        <div>Add to Cart</div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <Header />
+      <div className={styles.mainContainer}>
+        <div className={styles.detailsContainer}>
+          <img
+            src={product.image}
+            alt={product.name}
+            className={styles.image}
+          />
+          <div className={styles.discriptionContainer}>
+            <div className={styles.productName}>{product.name}</div>
+            <div className={styles.productPrice}>{product.price}</div>
+            <div>{product.description}</div>
+            <div className={styles.size}>Size</div>
+            <div className={styles.color}>Color</div>
+            <div className={styles.buttomContainer}>
+              <div className={styles.qunatity}>Quantity</div>
+              <button className={styles.button}>Add to Cart</button>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
